e5484f8c4584ddba35d7569b0ed00839cf94b5b6
Steve Kondik <shade@chemlab.org>
2013-10-14 12:25:08 -0700

    camera: Add parameter debugging support
    Change-Id: Ifc1653f0046af169ba36d94796b4d9a3055c6798

--- src/com/android/camera/PhotoModule.java
+++ src/com/android/camera/PhotoModule.java
@@ -4168,6 +4168,7 @@ public class PhotoModule
                 doModeSwitch = updateCameraParametersPreference();
             }
 
+            CameraUtil.dumpParameters(mParameters);
             mCameraDevice.setParameters(mParameters);
 
             // Switch to gcam module if HDR+ was selected
