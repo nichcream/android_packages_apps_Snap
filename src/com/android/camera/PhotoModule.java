368a6f1f6a0b191e39dfe9ee42b3031f203c1119
Dave Daynard <nardholio@gmail.com>
2014-09-02 19:14:08 -0400

    Camera2: Headset shutter mode
    Use play button on headset to take a picture/video/panorama
    Use media next/previous for zoom
    
    Change-Id: I25c22646b041e5e0941fbe361eccfe77a2a1e42f

--- src/com/android/camera/PhotoModule.java
+++ src/com/android/camera/PhotoModule.java
@@ -2743,6 +2743,7 @@ public class PhotoModule
         }
         switch (keyCode) {
             case KeyEvent.KEYCODE_VOLUME_UP:
+            case KeyEvent.KEYCODE_MEDIA_NEXT:
                 if (mFirstTimeInitialized && (mUI.mMenuInitialized)) {
                     if (!CameraActivity.mPowerShutter && !CameraUtil.hasCameraKey()) {
                         onShutterButtonFocus(true);
@@ -2752,6 +2753,7 @@ public class PhotoModule
                 }
                 return true;
             case KeyEvent.KEYCODE_VOLUME_DOWN:
+            case KeyEvent.KEYCODE_MEDIA_PREVIOUS:
                 if (mFirstTimeInitialized && (mUI.mMenuInitialized)) {
                     if (!CameraActivity.mPowerShutter && !CameraUtil.hasCameraKey()) {
                         onShutterButtonFocus(true);
@@ -2769,6 +2771,7 @@ public class PhotoModule
                 }
                 return false;
             case KeyEvent.KEYCODE_CAMERA:
+            case KeyEvent.KEYCODE_HEADSETHOOK:
                 if (mFirstTimeInitialized && event.getRepeatCount() == 0) {
                     onShutterButtonClick();
                 }
@@ -2799,6 +2802,8 @@ public class PhotoModule
         switch (keyCode) {
             case KeyEvent.KEYCODE_VOLUME_UP:
             case KeyEvent.KEYCODE_VOLUME_DOWN:
+            case KeyEvent.KEYCODE_MEDIA_NEXT:
+            case KeyEvent.KEYCODE_MEDIA_PREVIOUS:
                 if (!CameraActivity.mPowerShutter && !CameraUtil.hasCameraKey()
                         && mFirstTimeInitialized) {
                     onShutterButtonClick();
