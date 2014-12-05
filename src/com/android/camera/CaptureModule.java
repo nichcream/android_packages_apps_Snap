77f94fb3daa26c047eb2ec369f3b75aae0144b30
Paul Rohde <codelogic@google.com>
2014-12-05 12:17:15 -0800

    Drop new focus indicator into Camera2.
    * Create a new custom focus view that interacts with physical lens diopter changes.
    * Replace all occurances of the old focus indicator with the new one.
    
    Change-Id: Ia02646ce4d1eb059ecb8a1dfccc15dfc9c167e1b

--- src/com/android/camera/CaptureModule.java
+++ src/com/android/camera/CaptureModule.java
@@ -120,6 +120,7 @@ import com.android.camera.imageprocessor.filter.SharpshooterFilter;
 import com.android.camera.imageprocessor.filter.StillmoreFilter;
 import com.android.camera.imageprocessor.filter.UbifocusFilter;
 import com.android.camera.ui.CountDownView;
+import com.android.camera.ui.focus.FocusRing;
 import com.android.camera.ui.ModuleSwitcher;
 import com.android.camera.ui.ProMode;
 import com.android.camera.ui.RotateTextToast;
@@ -2935,7 +2936,7 @@ public class CaptureModule implements CameraModule, PhotoController,
                     @Override
                     public void run() {
                         if (mUI.getCurrentProMode() != ProMode.MANUAL_MODE)
-                            mUI.clearFocus();
+                        mUI.getFocusRing().stopFocusAnimations();
                     }
                 });
             }
@@ -3922,11 +3923,11 @@ public class CaptureModule implements CameraModule, PhotoController,
         int[] newXY = {x, y};
         if (mUI.isOverControlRegion(newXY)) return;
         if (!mUI.isOverSurfaceView(newXY)) return;
-        mUI.setFocusPosition(x, y);
+        mUI.getFocusRing().startActiveFocus();
+        mUI.getFocusRing().setFocusLocation(x, y);
         x = newXY[0];
         y = newXY[1];
         mInTAF = true;
-        mUI.onFocusStarted();
         if (isBackCamera()) {
             switch (getCameraMode()) {
                 case DUAL_MODE:
@@ -4429,7 +4430,7 @@ public class CaptureModule implements CameraModule, PhotoController,
                 return;
             }
 
-            mUI.clearFocus();
+            mUI.getFocusRing().stopFocusAnimations();
             mUI.resetPauseButton();
             mRecordingTotalTime = 0L;
             mRecordingStartTime = SystemClock.uptimeMillis();
@@ -4496,7 +4497,7 @@ public class CaptureModule implements CameraModule, PhotoController,
             mHandler.post(new Runnable() {
                 @Override
                 public void run() {
-                    mUI.clearFocus();
+                    mUI.getFocusRing().stopFocusAnimations();
                     mUI.resetPauseButton();
                     mRecordingTotalTime = 0L;
                     mRecordingStartTime = SystemClock.uptimeMillis();
@@ -4673,7 +4674,7 @@ public class CaptureModule implements CameraModule, PhotoController,
 
         try {
             setUpMediaRecorder(cameraId);
-            mUI.clearFocus();
+            mUI.getFocusRing().stopFocusAnimations();
             mUI.hideUIwhileRecording();
             mCameraHandler.removeMessages(CANCEL_TOUCH_FOCUS, mCameraId[cameraId]);
             if (isAbortCapturesEnable() && mCaptureSession[cameraId] != null) {
@@ -4773,7 +4774,7 @@ public class CaptureModule implements CameraModule, PhotoController,
                                         startRecordingFailed();
                                         return;
                                     }
-                                    mUI.clearFocus();
+                                    mUI.getFocusRing().stopFocusAnimations();
                                     mUI.resetPauseButton();
                                     mRecordingTotalTime = 0L;
                                     mRecordingStartTime = SystemClock.uptimeMillis();
